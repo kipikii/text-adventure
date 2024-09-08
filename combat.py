c98ecb19c1fc18f55d10688525958a93cb7bfd98
Lumin <troy.12178933@gmail.com>
2024-09-07 17:45:00 -0700

    added damage variation (modifer of .8-1.2x damage)

--- combat.py
+++ combat.py
@@ -15,13 +15,6 @@ inventory = {
     "throwing knife": 2
 }
 
-# change below string to change what monster you fight
-enemyName, stats = choice(list(monsters.items()))
-enemyHP = stats[0]
-enemySTR = stats[1]
-enemyDEX = stats[2]
-enemyDEF = stats[3]
-
 playerHP = 15
 playerMaxHP = playerHP
 playerSTR = 5
@@ -29,7 +22,7 @@ playerDEX = 2
 playerDEF = 0
 playerCharm = None
 
-valid = ["attack", "skill", "item"]
+valid = ["attack", "skill", "item", "a", "s", "i"]
 
 def verify(question):
     while (True):
@@ -47,32 +40,41 @@ def doCombat(playerHP, playerSTR, playerDEX, playerDEF, playerCharm, enemyName, 
     while (playerHP > 0 and enemyHP > 0):
         print("[Your HP: " + str(playerHP) + " / " + str(playerMaxHP) + "]")
         chosen = verify("what would you like to do? [attack, skill, item] ")
+
         # code for attacking
-        if (chosen == "attack"):
+        if (chosen == "attack" or chosen == "a"):
             # get the percentage chance for the player to hit
-            hitChance = round(playerDEX / (enemyDEX + 1) * 100)
+            hitChance = round(playerDEX / (enemyDEX + 1) * 100) + 10
             if (hitChance >= randint(1,100)):
-                print("you attacked the " + enemyName + " for " + str(playerSTR - enemyDEF) + " damage")
-                enemyHP -= playerSTR - enemyDEF
+                damage = round((playerSTR - enemyDEF) * (randint(80, 120)/100))
+                if (damage >= 0):
+                    print("you attacked the " + enemyName + " for " + str(damage) + " damage")
+                    enemyHP -= damage
+                else:
+                    print("you attacked the " + enemyName + " for 0 damage")
             else:
                 print("your attack missed")
 
         # code for using skills
-        elif (chosen == "skill"):
+        elif (chosen == "skill" or chosen == "s"):
             print("i'm definitely going to add skills")
 
         # code for using items
-        elif (chosen == "item"):
+        elif (chosen == "item" or chosen == "i"):
             print("inventory here or smth")
 
         # if the enemy is alive, have them attack
         if (enemyHP > 0):
             print(" ")
             print("the " + enemyName + " attacks")
-            hitChance = round(enemyDEX / (playerDEX + 1) * 100)
+            hitChance = round(enemyDEX / (playerDEX + 1) * 100) + 10
             if (hitChance >= randint(1,100)):
-                print("you took " + str(enemySTR - playerDEF) + " damage")
-                playerHP -= enemySTR - playerDEF
+                damage = round((enemySTR - playerDEF) * (randint(80, 120)/100))
+                if (damage >= 0):
+                    print("you took " + str(damage) + " damage")
+                    playerHP -= damage
+                else:
+                    print("you took 0 damage")
             else:
                 print("but its attack missed")
         if (enemyHP > 0):
@@ -80,17 +82,19 @@ def doCombat(playerHP, playerSTR, playerDEX, playerDEF, playerCharm, enemyName, 
     # figure out who's alive at the end of the fight
     if (enemyHP <= 0):
         print("the " + enemyName + " has been slain")
+        return playerHP
     else:
         print("you were slain...")
         quit()
 
 # forever fight creatures
 while (True):
-    doCombat(playerHP, playerSTR, playerDEX, playerDEF, playerCharm, enemyName, enemyHP, enemySTR, enemyDEX, enemyDEF)
-    print(" ")
-    print("as you defeat the beast, another creature leaps out at you")
+    # get a random enemy's name and stats from the "monsters" dictionary
     enemyName, stats = choice(list(monsters.items()))
     enemyHP = stats[0]
     enemySTR = stats[1]
     enemyDEX = stats[2]
     enemyDEF = stats[3]
+    playerHP = doCombat(playerHP, playerSTR, playerDEX, playerDEF, playerCharm, enemyName, enemyHP, enemySTR, enemyDEX, enemyDEF)
+    print(" ")
+    print("as you defeat the beast, another creature leaps out at you")
