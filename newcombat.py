b897a26ed872d6a9998613bf94e5033366f69a9e
Lumin <troy.12178933@gmail.com>
2024-10-17 12:19:21 -0700

    castSpell and calcHit added

--- newcombat.py
+++ newcombat.py
@@ -6,7 +6,8 @@ from time import *
 
 class Spell:
     # self, mana cost, number of casts, stat to use, power of attack (multiplier), chance to hit, damage take on cast (multiplier), effects to enemy, effects to caster, victim var, attacker var
-    def __init__(self, cost, procs, dmgStat, hitStat, damage, damageRecoil, applyEffect, givesEffect):
+    def __init__(self, name, cost, procs, dmgStat, hitStat, damage, damageRecoil, applyEffect, givesEffect):
+        self.name = name
         self.cost = cost
         self.damage = damage
         self.procs = procs
@@ -17,14 +18,16 @@ class Spell:
         self.givesEffect = givesEffect
 
 class Monster:
-    def __init__(self, name, HP, STR, DEX, DEF, skills: list):
+    def __init__(self, name: str, HP: int, STR: int, DEX: int, DEF: int, AGI: int, skills: list):
         self.name = name
         self.HP = HP
         self.MaxHP = self.HP
         self.MP = inf
+        self.MaxMP = inf
         self.STR = STR
         self.DEX = DEX
         self.DEF = DEF
+        self.AGI = AGI
         self.skills = skills
         self.status = [ ]
     def skillSelect(self, skills: list):
@@ -32,23 +35,27 @@ class Monster:
         print(toUse)
 
 class Equipment:
-    def __init__(self, slot, BonusHP, BonusMP, BonusSTR, BonusDEX, BonusDEF, special):
+    def __init__(self, slot, BonusHP, BonusMP, BonusSTR, BonusDEX, BonusDEF, BonusAGI, special):
         self.slot = slot
         self.BonusHP = BonusHP
         self.BonusMP = BonusMP
         self.BonusSTR = BonusSTR
         self.BonusDEX = BonusDEX
         self.BonusDEF = BonusDEF
+        self.BonusAGI = BonusAGI
         self.special = special
 
 class Player:
-    def __init__(self, HP: int, MP: int, STR: int, DEX: int, DEF: int):
+    def __init__(self, HP: int, MP: int, STR: int, DEX: int, DEF: int, AGI: int):
         self.level = 1
         self.HP = HP
+        self.MaxHP = HP
         self.MP = MP
+        self.MaxMP = MP
         self.STR = STR
         self.DEX = DEX
         self.DEF = DEF
+        self.AGI = AGI
         self.skills = [ ]
         self.status = [ ]
         self.weapon = None
@@ -57,13 +64,33 @@ class Player:
         self.legs = None
         self.charm = None
 
+def calcHit(victimAGI: int, attackerDEX: int):
+    # roll for agi proc. if fail, hit
+    if (victimAGI < 0): victimAGI = 0
+    if (attackerDEX < 0): attackerDEX = 0
+    if (victimAGI == 3): 
+        hitChance = round(log(((attackerDEX + 1) * (2.999999 / (victimAGI + 1.0000001)))) * 40) + 45
+    else: 
+        hitChance = round(log((attackerDEX + 1) * (3 / (victimAGI + 1.0000001))) * 40) + 45
+    if (hitChance < 30): hitChance = 30
+    if (hitChance >= randint(1,100)): return True
+    else: return False
+
 #cost, procs, dmgStat, hitStat, damage, damageRecoil, applyEffect, givesEffect
 #mana cost, number of casts, stat to use for dmg, stat to use for hit, power of attack (multiplier), chance to hit, damage take on cast (multiplier), effects to enemy, effects to caster, victim var, attacker var
 def castSpell(spell, caster, victim):
-    print("spell cost: " + str(spell.cost))
+    print("the " + caster.name + " attacks!")
     for each in range(spell.procs):
-        print("stat for hit: " + str(spell.hitStat) + ", " + str(eval("caster." + str(spell.hitStat))))
-        print("stat for damage: " + str(spell.dmgStat) + ", " + str(eval("caster." + str(spell.dmgStat))))
+        if (calcHit(eval("caster." + str(spell.hitStat)), victim.AGI)):
+            damage = round((eval("caster." + str(spell.dmgStat)) * (randint(90, 120)/100)) - victim.DEF)
+            if (damage <= 0):
+                print("0 damage")
+            else:
+                print(str(damage) + " damage")
+                victim.HP -= damage
+        else:
+            print("miss")
+    print("your hp: " + str(victim.HP) + " / " + str(victim.MaxHP))
 
 def verify(question:str=None, allowed:list=None):
     while (True):
@@ -79,18 +106,33 @@ def verify(question:str=None, allowed:list=None):
                 return chosen
 
 spells = {
-    "attack": Spell(0, 1, "STR", "DEX", 1, 0, [ ], [ ]),
-    "doublecut": Spell(5, 2, "STR", "DEX", 1.25, 0, [ ], [ ])
+    "attack": Spell("attack", 0, 1, "STR", "DEX", 1, 0, [ ], [ ]),
+    "doublecut": Spell("doublecut", 5, 2, "STR", "DEX", 1.25, 0, [ ], [ ]),
+    "bolt": Spell("bolt", 5, 1, "MP", "AGI", .5, 0, [], []),
 }
 
 monsters = {
-    "rat": Monster("rat", 3, 2, 0, 0, ["attack"]),
-    "wolf": Monster("wolf", 10, 3, 1, 1, ["attack"]),
-    "imp": Monster("imp", 15, 4, 5, -3, ["attack", "flame"])
+    "rat": Monster("rat", 3, 2, 0, 0, 0, ["attack"]),
+    "wolf": Monster("wolf", 10, 3, 3, 1, 0, ["attack"]),
+    "imp": Monster("imp", 15, 4, 5, -3, 0, ["attack", "bolt"]),
+    "dummy" : Monster("dummy", 100, 10, 10, 10, 10, ["attack"])
 }
 
-player = Player(100, 100, 10, 10, 10)
+player = Player(100, 10, 10, 10, 10, 10)
 
-enemy = monsters["wolf"]
-print("the " + enemy.name + " attacked for " + str(enemy.STR) + " damage")
+enemy = monsters["dummy"]
 castSpell(spells["attack"], enemy, player)
+
+hits = 0
+misses = 0
+print("")
+print("agi: " + str(player.AGI))
+print("dex: " + str(enemy.DEX))
+print('out of 10000 hits:')
+for i in range(10000):
+    x = calcHit(player.AGI, enemy.DEX)
+    if (x): hits += 1
+    else: misses += 1
+print("hits: " + str(hits))
+print("misses: " + str(misses))
+print("hit percentage: " + str(hits/100) + "%")
